5fc7a224227e6fa0fa67db54d9d32f764b9c1f5e
psicktrick <poojarysaurabh932@gmail.com>
2019-01-04 04:20:49 +0000

    Done

--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -1,3 +1,4 @@
+# %load q02_best_k_features/build.py
 # Default imports
 
 import pandas as pd
@@ -9,4 +10,30 @@ from sklearn.feature_selection import f_regression
 
 
 # Write your solution here:
+def percentile_k_features(df,k=20):
+    X=df.iloc[:,:-1]
+    y=df.iloc[:,-1]
+    sp = SelectPercentile(f_regression,percentile=k)
+    sp.fit_transform(X,y)
+    features = X.columns.values[sp.get_support()]
+    scores = sp.scores_[sp.get_support()]
+    fs_score = list(zip(features,scores))
+    df = pd.DataFrame(fs_score,columns=['Name','Score'])
+    return df.sort_values(['Score','Name'],ascending = [False,True])['Name'].tolist()
+
+
+
+X=data.iloc[:,:-1]
+y=data.iloc[:,-1]
+sp = SelectPercentile(f_regression,percentile=20)
+sp.fit_transform(X,y)
+
+features = X.columns.values[sp.get_support()]
+scores = sp.scores_[sp.get_support()]
+scores
+fs_score = list(zip(features,scores))
+df = pd.DataFrame(fs_score,columns=['Name','Score'])
+df.head()
+df.sort_values(['Score','Name'],ascending = [False,True])#['Name'].tolist()
+
 
